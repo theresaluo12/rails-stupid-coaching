e25e59c9dbe8e210c0fda30b658a4b7e42e1c13a
Theresa Luo <theresaa.luo@gmail.com>
2022-02-21 21:23:40 +1100

    stupid coaching

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,2 +1,17 @@
 class QuestionsController < ApplicationController
+  def answer
+
+      @input = params[:question]
+      if @input == 'I am going to work'
+        @answer = 'Great!'
+      elsif @input.include? "?"
+        @answer = 'Silly question, get dressed and go to work!'
+      else @answer = "I don't care, get dressed and go to work!"
+      end
+  end
+
+  def ask
+
+  end
+
 end
